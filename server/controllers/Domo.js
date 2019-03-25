482c38855a845aa303c7fb9de02209d87cd90a70
Paul Wilson <pjw6900@rit.edu>
2019-03-25 10:48:23 -0400

    domomaker-c

--- server/controllers/Domo.js
+++ server/controllers/Domo.js
@@ -4,13 +4,13 @@ const Domo = models.Domo;
 
 const makerPage = (req, res) => {
   Domo.DomoModel.findByOwner(req.session.account._id, (err, docs) => {
-    if(err) {
+    if (err) {
       console.log(err);
-      return res.status(400).json({error:'An error occurred'});
+      return res.status(400).json({ error: 'An error occurred' });
     }
-    
-    return res.render('app', {domos: docs});
-  })
+
+    return res.render('app', { csrfToken: req.csrfToken() }, { domos: docs });
+  });
 };
 
 
@@ -18,28 +18,28 @@ const makeDomo = (req, res) => {
   if (!req.body.name || !req.body.age) {
     return res.status(400).json({ error: 'Both name and age are required' });
   }
-  
+
   const domoData = {
     name: req.body.name,
     age: req.body.age,
     owner: req.session.account._id,
-  }
-  
+  };
+
   const newDomo = new Domo.DomoModel(domoData);
-  
+
   const domoPromise = newDomo.save();
-  
-  domoPromise.then(() => res.json({redirect: '/maker'}));
-  
+
+  domoPromise.then(() => res.json({ redirect: '/maker' }));
+
   domoPromise.catch((err) => {
     console.log(err);
-    if(err.code === 11000) {
+    if (err.code === 11000) {
       return res.status(400).json({ error: 'Domo already exists' });
     }
-    
+
     return res.status(400).json({ error: 'An error occurred' });
-  })
-  
+  });
+
   return domoPromise;
 };
 
